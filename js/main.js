23632a393bcf588a67116fef731315ef8fb93fc4
sajeewaml9930 <sajeewaml@gmail.com>
2023-05-21 18:31:05 +0530

    Change some parts

--- js/main.js
+++ js/main.js
@@ -1,31 +1,36 @@
-$(document).ready(function() {
-    console.log('Window Height is: ' + $(window).height());
-    console.log('Document Height is: ' + $(document).height());
-
-    //previousScroll
-
-    var previousScroll = 0;
-
-    $(window).scroll(function() {
-        var currentScroll = $(this).scrollTop();
-
-            //showing when scroll down
-            if (currentScroll > previousScroll) {
-                window.setTimeout(hidenav, 10); 
-            } 
-            //show when scrolling value 0
-            else if(currentScroll == 0) {
-                window.setTimeout(showNav, 10);
-            }
-
-            //scrolling down
-            function hidenav() {
-                $("#navigation").addClass("navbar-color");
-            }
-
-            //not scrolling and placed in the top
-            function showNav() {
-                $("#navigation").removeClass("navbar-color");
-            }
-    })
-});
+// set up text to print, each item in array is a new line
+var aText = new Array(
+    "THE FUTURE OF IOT IS HERE"
+  );
+  var iSpeed = 100; // time delay of print out
+  var iIndex = 0; // start printing array at this position
+  var iArrLength = aText[0].length; // the length of the text array
+  var iScrollAt = 20; // start scrolling up at this many lines
+  
+  var iTextPos = 0; // initialize text position
+  var sContents = ''; // initialize contents variable
+  var iRow; // initialize current row
+  
+  function typewriter() {
+    sContents = ' ';
+    iRow = Math.max(0, iIndex - iScrollAt);
+    var destination = document.getElementById("typedtext");
+  
+    while (iRow < iIndex) {
+      sContents += aText[iRow++] + '<br />';
+    }
+    destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "";
+    if (iTextPos++ == iArrLength) {
+      iTextPos = 0;
+      iIndex++;
+      if (iIndex != aText.length) {
+        iArrLength = aText[iIndex].length;
+        setTimeout(typewriter, 500);
+      }
+    } else {
+      setTimeout(typewriter, iSpeed);
+    }
+  }
+  
+  typewriter();
+  
